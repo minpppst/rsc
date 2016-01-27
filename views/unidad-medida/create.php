ba62870eaa417dbb20d9c8567026e553e84f347a
Catu52 <catu52@gmail.com>
2016-01-27 10:23:04 -0430

    Unidad de Medida

---
+++ views/unidad-medida/create.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\UnidadMedida */
+
+?>
+<div class="unidad-medida-create">
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+</div>
