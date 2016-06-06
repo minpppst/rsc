0bae005cf727a3ef519a3747f0f9b4b589503696
walter jimenez <walter86.79@gmail.com>
2016-06-06 11:22:03 -0400

    menu frontend variables

--- frontend/views/layouts/_items.php
+++ frontend/views/layouts/_items.php
@@ -4,8 +4,13 @@
 
     //Puede ver el item "requerimientos"
     $req = Yii::$app->user->can('proyecto-pedido/index') || Yii::$app->user->can('accion-centralizada-pedido/index') ? true : false;
-
-	/*
+    $variables = Yii::$app->user->can('proyecto-variables/index') || Yii::$app->user->can('accion-centralizada-variables/index') ? true : false;
+        if($variables){
+        $variablesp = Yii::$app->user->can('proyecto-variables/index') ? true : false;
+        $variablesa = Yii::$app->user->can('accion-centralizada-variables/index') ? true : false;
+        }
+	
+    /*
 	 * Items del menu principal
 	 */    
 
@@ -14,6 +19,10 @@
             ['label' => $icons['pedido'].' Requerimientos','visible' => $req, 'items' =>[
                 ['label' => $icons['proyecto'].' Proyecto', 'url' => ['/proyecto-pedido/index']],
                 ['label' => $icons['acc'].' Acción Centralizada', 'url' => ['/accion-centralizada-pedido/index']],
+            ]],
+            ['label' => $icons['pedido'].' Variables','visible' => $variables, 'items' =>[
+                ['label' => $icons['proyecto'].' Proyecto', 'visible' => $variablesp, 'url' => ['/proyecto-variables/index']],
+                ['label' => $icons['acc'].' Acción Centralizada',  'visible' => $variablesa, 'url' => ['/accion-centralizada-variables/index']],
             ]],            
             ['label' => $icons['proyecto'].' Proyecto', 'url' => ['/proyecto/index'], 'visible' => Yii::$app->user->can('proyecto/index')],
             ['label' => $icons['acc'].' Acción Centralizada', 'url' => ['/accion-centralizada'], 'visible' => Yii::$app->user->can('accion-centralizada/index')],                        
