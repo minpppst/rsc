8c19766b31a6cf44761d13477a2ff801583240c6
Carlos Samaniego <catu52@gmail.com>
2016-08-20 21:57:13 -0400

    Colocando el boton para la programacion fisica de la accion especifica de proyecto. Issue #53

--- frontend/views/proyecto-accion-especifica/_columns.php
+++ frontend/views/proyecto-accion-especifica/_columns.php
@@ -104,15 +104,27 @@ return [
         'urlCreator' => function($action, $model, $key, $index) { 
                 return Url::to([$action,'id'=>$key]);
         },
-        'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
-        'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
-        'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
+        'viewOptions'=>['role'=>'modal-remote','title'=>'Ver','data-toggle'=>'tooltip'],
+        'updateOptions'=>['role'=>'modal-remote','title'=>'Editar', 'data-toggle'=>'tooltip'],
+        'deleteOptions'=>['role'=>'modal-remote','title'=>'Eliminar', 
                           'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
                           'data-request-method'=>'post',
                           'data-toggle'=>'tooltip',
-                          'data-confirm-title'=>'Are you sure?',
-                          'data-confirm-message'=>'Are you sure want to delete this item',
+                          'data-confirm-title'=>'¿Está seguro?',
+                          'data-confirm-message'=>'¿Está seguro que desea eliminar este item?',
                           'class' => 'text-danger'], 
+        'template' => '{view} {update} {prg} {delete}',
+        'buttons' => [
+            'prg' => function(){
+                return Html::a('<span class="glyphicon glyphicon-calendar"></span>', '#', [
+                    'role'=>'modal-remote',
+                    'title'=>'Programación',
+                    'data-toggle'=>'tooltip',
+                    'data-confirm-title'=>'¿Está Seguro?',
+                    'data-confirm-message'=>'¿Desea continuar?'
+                ]);
+            }
+        ]
     ],
 
 ];   
